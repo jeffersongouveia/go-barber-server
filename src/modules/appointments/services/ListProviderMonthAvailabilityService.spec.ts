d0113dd9b1208ac4d055357df89aad3b9547b74f
Jefferson Gouveia <jeffersongouveia@eucatur.com.br>
2020-10-27 11:00:17 -0400

    ✅ Fixed the test, provider id was wrong

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -12,21 +12,11 @@ describe('ListProviderMonthAvailability', () => {
 
   it('should be able to list the month availability from provider', async () => {
     const appointmentsResponses = []
-    let response
-
-    response = fakeAppointmentsRepository.create({
-      provider_id: 'provider-id',
-      user_id: 'user-id',
-      date: new Date(2020, 11, 20, 10, 0, 0),
-    })
-
-    appointmentsResponses.push(response)
-
     for (let i = 8; i < 18; i++) {
-      response = fakeAppointmentsRepository.create({
+      const response = fakeAppointmentsRepository.create({
         provider_id: 'provider-id',
         user_id: 'user-id',
-        date: new Date(2020, 11, 21, i, 0, 0),
+        date: new Date(2020, 11, 4, i, 0, 0),
       })
 
       appointmentsResponses.push(response)
@@ -35,17 +25,17 @@ describe('ListProviderMonthAvailability', () => {
     await Promise.all(appointmentsResponses)
 
     const availability = await listProvidersMonthly.execute({
-      provider_id: 'user-id',
+      provider_id: 'provider-id',
       year: 2020,
-      month: 11,
+      month: 12,
     })
 
     expect(availability).toEqual(
       expect.arrayContaining([
-        { day: 19, available: true },
-        { day: 20, available: true },
-        { day: 21, available: false },
-        { day: 22, available: true },
+        { day: 2, available: true },
+        { day: 3, available: true },
+        { day: 4, available: false },
+        { day: 5, available: true },
       ]))
   })
 })
