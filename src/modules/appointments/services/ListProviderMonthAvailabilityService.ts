a985d4318fe9389e8ef23c78eebeb9eda1eb6d4a
Jefferson Gouveia <jeffersongouveia@eucatur.com.br>
2020-10-30 11:40:46 -0400

    ✅ Fixing the availability in month route

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
 
 import { inject, injectable } from 'tsyringe'
-import { getDate, getDaysInMonth } from 'date-fns'
+import { getDate, getDaysInMonth, isAfter } from 'date-fns'
 
 import IAppointmentsRepository from '@modules/appointments/infra/repositories/IAppointmentsRepository'
 
@@ -28,21 +28,24 @@ class ListProviderMonthAvailabilityService {
   }
 
   public async execute(data: IRequest): Promise<IResponse> {
+    const { month, year } = data
+
     const appointments = await this.repository.findAllInMonthFromProvider(data)
 
-    const numberOfDaysInMonth = getDaysInMonth(new Date(data.year, data.month - 1))
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1))
     const allDays = Array.from(
       { length: numberOfDaysInMonth },
       (_, index) => index + 1,
     )
 
     const availability = allDays.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59)
       const appointmentInDay = appointments.filter((appointment) => getDate(appointment.date) === day)
 
       return {
         day,
         // Das 8 até as 17h apenas
-        available: appointmentInDay.length < 10,
+        available: isAfter(compareDate, new Date()) && appointmentInDay.length < 10,
       }
     })
 
